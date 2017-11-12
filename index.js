647b0691bdd8101ecf9badfde3830c581e296e3f
Derepa Alexey <graf_dav2@ukr.net>
2017-11-12 14:10:00 +0200

    fix bugs

--- index.js
+++ index.js
@@ -1,66 +1,39 @@
-/* participantObject EXAMPLE */
-/* Example: { firstName: 'Sergey', lastName: 'Zotenko', seniorityLevel: 'intermediate' } */
-// const participantObject = {
-//     firstName: string,
-//     lastName: string,
-//     seniorityLevel: string
-// }
-
-/* pricingObject EXAMPLE */
-/* Example: { 'junior': 10 } */
-// const pricingObject = {
-//     roleName: number
-// }
-
 const project = {
     participants: [],
     pricing: {},
     isBusy: false,
 
-    /* implement initialization of the object */
-    /* participants - predefined array of participants */
-    /* pricing - predefined object (keyvalue collection) of pricing */
     init(participants, pricing) {
-        if (participants instanceof Array)
+        if (participants instanceof Array
+            && participants.every(item => item.hasOwnProperty('seniorityLevel')))
             this.participants = participants;
         this.pricing = pricing;
     },
 
-    /* pass found participant into callback, stops on first match */
-    /* functor - function that will be executed for elements of participants array */
-    /* callbackFunction - function that will be executed with found participant as argument or with null if not */
-    /* callbackFunction (participant) => {} */
     findParticipant(functor, callbackFunction) {
 
         this.isBusy = true;
-
+        let participant = null;
         setTimeout(() => {
 
-            let participant = this.participants.find(functor);
+            participant = this.participants.find(functor);
             if (participant == undefined)
                 participant = null;
 
-            callbackFunction(participant);
             this.isBusy = false;
+            callbackFunction(participant);
         });
     },
 
-    /* pass array of found participants into callback */
-    /* functor - function that will be executed for elements of participants array */
-    /* callbackFunction - function that will be executed with array of found participants as argument or empty array if not */
-    /* callbackFunction (participantsArray) => {} */
     findParticipants(functor, callbackFunction) {
         this.isBusy = true;
         setTimeout(() => {
             let partOfparticipants = this.participants.filter(functor);
-            callbackFunction(partOfparticipants);
             this.isBusy = false;
+            callbackFunction(partOfparticipants);
         });
     },
 
-    /* push new participant into this.participants array */
-    /* callbackFunction - function that will be executed when job will be done */
-    /* (err) => {} */
     addParticipant(participantObject, callbackFunction) {
         this.isBusy = true;
         setTimeout(() => {
@@ -69,22 +42,19 @@ const project = {
                     && typeof participantObject === 'object'
                     && "seniorityLevel" in participantObject) {
                     this.participants.push(participantObject);
+                    this.isBusy = false;
                     callbackFunction();
                 } else {
                     throw new Error(err)
                 }
             } catch (error) {
+                this.isBusy = false;
                 callbackFunction(error);
             }
             this.isBusy = false;
         });
     },
 
-
-
-    /* push new participant into this.participants array */
-    /* callback should receive removed participant */
-    /* callbackFunction - function that will be executed with object of removed participant or null if participant wasn't found when job will be done */
     removeParticipant(participantObject, callbackFunction) {
         this.isBusy = true;
         setTimeout(() => {
@@ -103,20 +73,20 @@ const project = {
                             break;
                         }
                     }
+                    this.isBusy = false;
 
                     callbackFunction(removedParticipant);
                 } else {
                     throw new Error(err)
                 }
             } catch (error) {
+                this.isBusy = false;
                 callbackFunction(null);
             }
             this.isBusy = false;
         });
     },
 
-    /* Extends this.pricing with new field or change existing */
-    /* callbackFunction - function that will be executed when job will be done, doesn't take any arguments */
     setPricing(participantPriceObject, callbackFunction) {
         this.isBusy = true;
         setTimeout(() => {
@@ -127,8 +97,6 @@ const project = {
         });
     },
 
-    /* calculates salary of all participants in the given period */
-    /* periodInDays, has type number, one day is equal 8 working hours */
     calculateSalary(periodInDays) {
         const workingHours = 8;
         let commonSalary = 0;
@@ -143,7 +111,7 @@ const project = {
         }
         result = commonSalary * workingHours * periodInDays;
         return result;
-    }   
+    }
 }
 
 
